3105c0d0e51572af5d38b0af21ab4e27434d5237
Eric Brian Anil <38664231+EricBrianAnil@users.noreply.github.com>
2023-04-19 09:22:16 +0530

    Update alert.py

--- alert.py
+++ alert.py
@@ -14,9 +14,9 @@ client = Client(account_sid, auth_token)
 # )
 
 client.api.account.messages.create(
-  from_='+15076051584',
+  from_='+1222222222',
   body='ALERT: Eric is in a state of INSOBRIERTY near TATA VALUE HOMES, CHENNAI at 02:22 AM IST',
-  to='+916282669846'
+  to='+91000000000'
 )
 
 #print(message.sid)
